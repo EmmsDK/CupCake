9246ca485d0a3ea75e80f6fc80c8eac21cf2be42
Johan M <cph-jm390@cphbusiness.dk>
2022-11-08 14:07:50 +0100

    bottom, order, topping adds

---
+++ CupCake/src/main/java/dat/backend/model/entities/Bottom.java
@@ -0,0 +1,13 @@
+package dat.backend.model.entities;
+
+public class Bottom {
+    int idBottom;
+    int bottomVar;
+    int bottomPrice;
+
+    public Bottom(int idBottom, int bottomVar, int bottomPrice) {
+        this.idBottom = idBottom;
+        this.bottomVar = bottomVar;
+        this.bottomPrice = bottomPrice;
+    }
+}
---
+++ CupCake/src/main/java/dat/backend/model/entities/Topping.java
@@ -0,0 +1,13 @@
+package dat.backend.model.entities;
+
+public class Topping {
+   int idTopping;
+   int toppingVar;
+   int toppingPrice;
+
+    public Topping(int idTopping, int toppingVar, int toppingPrice) {
+        this.idTopping = idTopping;
+        this.toppingVar = toppingVar;
+        this.toppingPrice = toppingPrice;
+    }
+}
